f209616ab6cfb189106e5ad1856e0406a733b824
ted <ted9904210ya@gmail.com>
2018-04-11 16:31:52 +0800

    Complete Ch0-Labnew

---
+++ Ch0-Labnew/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch0_Labnew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+    }
+}
